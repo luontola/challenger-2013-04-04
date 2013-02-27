edc9f77c6270da8da62df8491d60bd2a564fc237
Esko Luontola <esko.luontola@solita.fi>
2013-02-27 17:57:52 +0200

    HTTP client

---
+++ src/esko_challenger/http.clj
@@ -0,0 +1,34 @@
+(ns esko-challenger.http
+  (:require [http.async.client :as http])
+  (:import [org.slf4j LoggerFactory Logger]))
+
+(defn- nil-or-str
+  [object]
+  (if (nil? object)
+    nil
+    (str object)))
+
+(defn- simple-http-response
+  [response]
+  {:body (http/string response)
+   :status (http/status response)
+   :error (nil-or-str (http/error response))})
+
+(defonce ^:private client (http/create-client))
+
+(defn ^:dynamic post-request
+  [url body]
+  (try
+    (-> (http/POST client url :body body :timeout 100)
+      http/await
+      simple-http-response)
+    (catch InterruptedException e
+      (throw e))
+    (catch Throwable t
+      {:body nil
+       :status nil
+       :error (.toString t)})))
+
+(defn error?
+  [response]
+  (not (nil? (:error response))))
---
+++ test/esko_challenger/http_test.clj
@@ -0,0 +1,11 @@
+(ns esko-challenger.http-test
+  (:use clojure.test)
+  (:require [esko-challenger.http :as http]))
+
+(deftest post-request-test
+
+  (testing "Returns an error message on internal network library error"
+    (let [response (http/post-request "<malformed url>" "")]
+      (is (nil? (:body response)))
+      (is (nil? (:status response)))
+      (is (re-matches #".*IllegalArgumentException.*" (:error response))))))
